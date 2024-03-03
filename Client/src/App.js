977e25102c5accfa2859e3a3bb60f30699293e1c
Arham <arhamansari454647@gmail.com>
2024-03-03 11:11:29 +0500

    Logo and Title Change

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Image from "./Assets/Images/logo.png"
 function App() {
   return (
     <div>
       <h1>This is my E-commerce App</h1>
+      <img src={Image} />
     </div>
   );
 }
