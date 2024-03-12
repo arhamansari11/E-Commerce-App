93ec4c681e340826d240695cf22eb8870458b567
Arham <arhamansari454647@gmail.com>
2024-03-12 13:15:22 +0500

    Bootstrap added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
-import Image from "./Assets/Images/logo.png"
+import Image from "./Assets/Images/logo.png";
 function App() {
   return (
     <div>
       <h1>This is my E-commerce App</h1>
-      <img src={Image} />
+      {/* <img classname="w-20" src={Image} /> */}
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
     </div>
   );
 }
